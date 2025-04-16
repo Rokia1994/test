9a40c972c3b34cde94050d69dce65dd9bf3784e9
ourida <ouridasiam@gmail.com>
2025-04-16 15:29:21 +0100

    commit apres modification config

--- config.php
+++ config.php
@@ -4,19 +4,6 @@
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-// Ensuite tu peux utiliser $_ENV ou getenv()
-/*$conn = mysqli_connect(
-    $_ENV['DB_HOST'],
-    $_ENV['DB_USER'],
-    $_ENV['DB_PASS'],
-    $_ENV['DB_NAME']
-);*/
-
-//$env = parse_ini_file('.env');
-
-// Informations d'identification define('DB_SERVER', 'localhost');
-   
- 
 
 define('DB_SERVER' , $_ENV['DB_HOST']);
 define('DB_NAME' , $_ENV['DB_NAME']);
